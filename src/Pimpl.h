827a6f5b72598b5d9e14105d632ce8689b6dd431
Vitaly Baldeev <vsbaldeev@gmail.com>
2018-04-01 11:58:32 +0300

    Added asignment and move operators and constructors

--- src/Pimpl.h
+++ src/Pimpl.h
@@ -7,7 +7,26 @@ template <typename T>
 class Pimpl {
 public:
     explicit Pimpl(T* ptr) : ptr_(ptr) {}
+
     explicit Pimpl(std::unique_ptr<T>&& ptr) : ptr_(std::move(ptr)) {}
+
+    Pimpl(const Pimpl& other) = delete;
+
+    Pimpl(Pimpl&& other) {
+        if (this != &other) {
+            ptr_ = std::move(other.ptr_);
+        }
+    }
+
+    Pimpl& operator=(const Pimpl& other) = delete;
+
+    Pimpl& operator=(Pimpl&& other) {
+        if (this != &other) {
+            ptr_ = std::move(other.ptr_);
+        }
+
+        return *this;
+    }
 
     T* operator->() {
         return ptr_.get();
